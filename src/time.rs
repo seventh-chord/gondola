7252eed5f31a1d12e5ceee655bbb62b20d7762de
Morten H. Solvang <morten.hauke.solvang@gmail.com>
2017-09-29 14:10:08 +0200

    Associated consts in time, Timing to Time

--- src/time.rs
+++ src/time.rs
@@ -20,7 +20,7 @@ impl Timer {
     }
 
     /// Returns `(time_since_start, time_since_last_tick)`
-    pub fn tick(&mut self) -> (Timing, Timing) {
+    pub fn tick(&mut self) -> (Time, Time) {
         let now = Instant::now();
 
         let age = (now - self.start).into();
@@ -33,14 +33,14 @@ impl Timer {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Timing(pub u64); 
+pub struct Time(pub u64); 
 
-impl Timing {
-    pub fn zero() -> Timing { Timing(0) }
+impl Time {
+    pub const ZERO: Time = Time(0);
 
-    pub fn from_ms(ms: u64) -> Timing { Timing(ms * 1_000_000) } 
-    pub fn from_secs(s: u64) -> Timing { Timing(s * 1_000_000_000) } 
-    pub fn from_secs_float(s: f32) -> Timing { Timing((s * 1_000_000_000.0) as u64) } 
+    pub fn from_ms(ms: u64) -> Time { Time(ms * 1_000_000) } 
+    pub fn from_secs(s: u64) -> Time { Time(s * 1_000_000_000) } 
+    pub fn from_secs_float(s: f32) -> Time { Time((s * 1_000_000_000.0) as u64) } 
 
     /// Converts this timing to seconds, truncating any overflow. 1.999 ms will be converted to 1 ms.
     pub fn as_ms(self) -> u64 { self.0 / 1_000_000 }
@@ -50,49 +50,49 @@ impl Timing {
 
     pub fn as_secs_float(self) -> f32 { self.0 as f32 / 1_000_000_000.0 }
 
-    pub fn max(self, other: Timing) -> Timing {
+    pub fn max(self, other: Time) -> Time {
         ::std::cmp::max(self, other)
     }
 
-    pub fn min(self, other: Timing) -> Timing {
+    pub fn min(self, other: Time) -> Time {
         ::std::cmp::min(self, other)
     }
 }
 
-impl Add for Timing {
-    type Output = Timing;
-    fn add(self, rhs: Timing) -> Timing {
-        Timing(self.0 + rhs.0)
+impl Add for Time {
+    type Output = Time;
+    fn add(self, rhs: Time) -> Time {
+        Time(self.0 + rhs.0)
     }
 }
 
-impl Sub for Timing {
-    type Output = Timing;
-    fn sub(self, rhs: Timing) -> Timing {
-        Timing(self.0 - rhs.0)
+impl Sub for Time {
+    type Output = Time;
+    fn sub(self, rhs: Time) -> Time {
+        Time(self.0 - rhs.0)
     }
 }
 
-impl AddAssign for Timing {
-    fn add_assign(&mut self, rhs: Timing) {
+impl AddAssign for Time {
+    fn add_assign(&mut self, rhs: Time) {
         self.0 += rhs.0;
     }
 }
 
-impl SubAssign for Timing {
-    fn sub_assign(&mut self, rhs: Timing) {
+impl SubAssign for Time {
+    fn sub_assign(&mut self, rhs: Time) {
         self.0 -= rhs.0;
     }
 }
 
-impl From<Duration> for Timing {
-    fn from(d: Duration) -> Timing {
-        Timing(d.as_secs()*1_000_000_000 + (d.subsec_nanos() as u64))
+impl From<Duration> for Time {
+    fn from(d: Duration) -> Time {
+        Time(d.as_secs()*1_000_000_000 + (d.subsec_nanos() as u64))
     }
 }
 
-impl From<Timing> for Duration {
-    fn from(t: Timing) -> Duration {
+impl From<Time> for Duration {
+    fn from(t: Time) -> Duration {
         let nanos = t.0 % 1_000_000_000;
         let secs = t.0 / 1_000_000_000;
         Duration::new(secs, nanos as u32)
