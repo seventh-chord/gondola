2758f2115d8670aa45d348ff68aafa50415e7f25
Morten H. Solvang <morten.hauke.solvang@gmail.com>
2017-10-05 13:12:08 +0200

    Use consts in Time

--- src/time.rs
+++ src/time.rs
@@ -32,23 +32,40 @@ impl Timer {
     }
 }
 
+/// Time, stored as nanoseconds
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Time(pub u64); 
 
 impl Time {
     pub const ZERO: Time = Time(0);
+    pub const NANOSECONDS_PER_SECOND: u64 = 1_000_000_000;
+    pub const NANOSECONDS_PER_MILISECOND: u64 = 1_000_000;
 
-    pub fn from_ms(ms: u64) -> Time { Time(ms * 1_000_000) } 
-    pub fn from_secs(s: u64) -> Time { Time(s * 1_000_000_000) } 
-    pub fn from_secs_float(s: f32) -> Time { Time((s * 1_000_000_000.0) as u64) } 
+    pub fn from_ms(ms: u64) -> Time {
+        Time(ms * Time::NANOSECONDS_PER_MILISECOND) 
+    }
+
+    pub fn from_secs(s: u64) -> Time {
+        Time(s * Time::NANOSECONDS_PER_SECOND) 
+    }
+
+    pub fn from_secs_float(s: f32) -> Time {
+        Time((s * Time::NANOSECONDS_PER_SECOND as f32) as u64) 
+    }
 
     /// Converts this timing to seconds, truncating any overflow. 1.999 ms will be converted to 1 ms.
-    pub fn as_ms(self) -> u64 { self.0 / 1_000_000 }
+    pub fn as_ms(self) -> u64 {
+        self.0 / Time::NANOSECONDS_PER_MILISECOND 
+    }
 
     /// Converts this timing to seconds, truncating any overflow. 1.999 seconds will be converted to 1.
-    pub fn as_secs(self) -> u64 { self.0 / 1_000_000_000 }
+    pub fn as_secs(self) -> u64 {
+        self.0 / Time::NANOSECONDS_PER_SECOND 
+    }
 
-    pub fn as_secs_float(self) -> f32 { self.0 as f32 / 1_000_000_000.0 }
+    pub fn as_secs_float(self) -> f32 {
+        self.0 as f32 / (Time::NANOSECONDS_PER_SECOND as f32) 
+    }
 
     pub fn max(self, other: Time) -> Time {
         ::std::cmp::max(self, other)
@@ -87,14 +104,14 @@ impl SubAssign for Time {
 
 impl From<Duration> for Time {
     fn from(d: Duration) -> Time {
-        Time(d.as_secs()*1_000_000_000 + (d.subsec_nanos() as u64))
+        Time(d.as_secs()*Time::NANOSECONDS_PER_SECOND + (d.subsec_nanos() as u64))
     }
 }
 
 impl From<Time> for Duration {
     fn from(t: Time) -> Duration {
-        let nanos = t.0 % 1_000_000_000;
-        let secs = t.0 / 1_000_000_000;
+        let nanos = t.0 % Time::NANOSECONDS_PER_SECOND;
+        let secs = t.0 / Time::NANOSECONDS_PER_SECOND;
         Duration::new(secs, nanos as u32)
     }
 }
